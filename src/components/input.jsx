96388a6f0a37def8d4592bb3ca21050ea886c527
krantipariyar574388-netizen <krantipariyar574388@gmail.com>
2026-08-03 06:58:02 +0545

    add : onChange handle

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,37 +1,3 @@
-// const Input = ({ onChange, label, id, placeholder, type = "text" }) => {
-//   return (
-//     <div
-//       style={{
-//         display: "flex",
-//         flexDirection: "column",
-//         gap: "10px",
-//         textAlign: "start",
-//         fontSize: "20px",
-//       }}
-//     >
-//       {/* label */}
-//       <label htmlFor={id}>{label}</label>
-//       {/* input  */}
-//       <input
-//         style={{
-//           padding: "12px 10px",
-//           fontSize: "20px",
-//           border: "1px solid indigo",
-//           borderRadius: "8px",
-//         }}
-//         onChange={onChange}
-//         id={id}
-//         placeholder={placeholder}
-//         type={type}
-//       />
-//     </div>
-//   );
-// };
-
-// export default Input;
-
-
-
 const Input = ({ onChange, label, id, name, placeholder, type = 'text' }) => {
     return (
         <div
